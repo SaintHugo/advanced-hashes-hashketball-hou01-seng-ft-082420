98abdc5a34b4d37b85ccade059822b2f5a71e66e
Hugo Rodrigez <hugor37@gmail.com>
2020-08-18 21:16:13 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -195,18 +195,18 @@ def player_stats(name)
   end
 
   def big_shoe_rebounds
-    playerrebound = nil 
+    playerrebound = nil
     numbers = []
-    biggest_shoesize = nil 
+    biggest_shoesize = nil
         game_hash.collect do |team, stats|
           stats[:players].collect do |player, data|
             numbers.push(data[:shoe])
-            biggest_shoesize = numbers.sort[-1] 
+            biggest_shoesize = numbers.sort[-1]
                data.collect do |category, stat|
-                 if category == :shoe 
+                 if category == :shoe
                   if stat == biggest_shoesize
                     playerrebound =  data[:rebounds]
-                  end 
+                  end
                 end
               end
             end
